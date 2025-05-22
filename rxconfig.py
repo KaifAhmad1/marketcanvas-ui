d6f647c728ce45b56b3b168a170be2ca2fd18317
Mohd Kaif <98801504+KaifAhmad1@users.noreply.github.com>
2025-05-22 13:36:24 +0530

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,7 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="app",
+    app_name="marketcanvas-ui",
     frontend_port=3000,
     backend_port=8000,
     api_url="http://localhost:8000",
